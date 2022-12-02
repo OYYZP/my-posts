c04484fb31472972f2d7981e2b1a0b97bdcab4d8
“oyzp” <183791003497@163.com>
2022-12-02 17:07:50 +0800

    配置sass环境

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+// import './App.css';
 function App() {
   return (
     <div className="App">
-       app
+      app
+
+      <div className="tr">fsdf</div>
     </div>
   );
 }
